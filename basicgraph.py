e991225b16d2b8d97bd83b5e3871cb19daca1b32
dod12345 <31848207+dod12345@users.noreply.github.com>
2023-02-13 15:07:35 +0200

    Add files via upload

---
+++ basicgraph.py
@@ -0,0 +1,22 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+from collections import defaultdict
+
+# This class represents a directed graph
+# using adjacency list representation
+
+
+class Graph:
+
+	# Constructor
+	def __init__(self):
+
+		# default dictionary to store graph
+		self.graph = defaultdict(list)
+
+	# function to add an edge to graph
+	def addEdge(self, u, v):
+		self.graph[u].append(v)
+
+
