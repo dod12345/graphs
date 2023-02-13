2b037ce1304eef5274fa77fd1f11f33c03902cdf
dod12345 <31848207+dod12345@users.noreply.github.com>
2023-02-13 15:22:25 +0200

    Update basicgraph.py

--- basicgraph.py
+++ basicgraph.py
@@ -18,5 +18,11 @@ class Graph:
 	# function to add an edge to graph
 	def addEdge(self, u, v):
 		self.graph[u].append(v)
-
-
+		
+		
+def find_parent(self, parent, i):
+        if parent[i] == i:
+            return i
+        if parent[i] != i:
+            return self.find_parent(parent, parent[i])
+		
